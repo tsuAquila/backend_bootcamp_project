a821c408ab64f3156f5b5051b360664d3c4f7277
Aquila <reenacottage@gmail.com>
2022-02-06 18:35:41 +0530

    initial commit

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://localhost:27017/to-do-list-task",
+  "mongodb+srv://aquila:1239@cluster0.rjimv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true },
   (err) => {
     if (!err) {
